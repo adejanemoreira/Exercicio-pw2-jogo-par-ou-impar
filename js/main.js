3cd09e72b0e0d5270f49eb67890a1b8ad67a57b0
@a.d.j.a.n.e <adjanead1@gmail.com>
2023-02-08 00:02:33 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,44 +2,42 @@ const inputNumber = document.querySelector("#num");
 const btnSend = document.querySelector("#send");
 const result = document.querySelector("#result");
 
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const getWinnerMessage = (playerNumber, machineNumber, playerChoice) => {
+  const total = playerNumber + machineNumber;
+  if (total % 2 === 0 && playerChoice === "Par") {
+    return `
+    A máquina escolheu o número: ${machineNumber}
+    Você escolheu o número: ${playerNumber}
+    Você escolheu: ${playerChoice}
+    ---------- Você ganhou! ----------`;
+  } else if (total % 2 === 1 && playerChoice === "Ímpar") {
+    return `
+    A máquina escolheu o número: ${machineNumber}
+    Você escolheu o número: ${playerNumber}
+    Você escolheu: ${playerChoice}
+    ---------- Você ganhou! ----------`;
+  } else {
+    return `
+    A máquina escolheu o número: ${machineNumber}
+    Você escolheu o número: ${playerNumber}
+    Você escolheu: ${playerChoice}
+    ---------- Você perdeu! ----------`;
+  }
+};
+
 const send = function (e) {
   e.preventDefault();
-
   const btnRadioImpar = document.querySelector("#impar");
   const btnRadioPar = document.querySelector("#par");
   const min = 1;
   const max = 10;
-  let num1 = Number(inputNumber.value);
-  let num2 = Number(Math.floor(Math.random() * (max - min) + min));
-  let soma = num1 + num2;
-
-  if (num1 <= 10) {
-    if (btnRadioImpar.checked == true || btnRadioPar.checked == true) {
-      if (soma % 2 == 0 && btnRadioPar.checked == true) {
-        result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
-        Você colorou: ${btnRadioPar.value}
-        ---------- Você ganhou! ----------`;
-      } else if (soma % 2 == 1 && btnRadioImpar.checked == true) {
-        result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
-        Você colorou: ${btnRadioImpar.value}
-        ---------- Você ganhou! ----------`;
-      } else if (soma % 2 == 0 && btnRadioPar.checked == false) {
-        result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
-        Você colorou: ${btnRadioImpar.value}
-        ---------- Você perdeu! ----------`;
-      } else if (soma % 2 == 1 && btnRadioImpar.checked == false) {
-        result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
-        Você colorou: ${btnRadioPar.value}
-        ---------- Você perdeu! ----------`;
-      }
+  const playerNumber = Number(inputNumber.value);
+  if (playerNumber <= 10) {
+    const machineNumber = getRandomNumber(min, max);
+    if (btnRadioImpar.checked || btnRadioPar.checked) {
+      const playerChoice = btnRadioImpar.checked ? "Ímpar" : "Par";
+      result.innerText = getWinnerMessage(playerNumber, machineNumber, playerChoice);
     } else {
       result.innerText = "Escolha Ímpar ou Par";
     }
